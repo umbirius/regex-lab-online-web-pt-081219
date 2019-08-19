75809f2b7aefd41321888d32ac2c4e3af99ea3ef
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 23:19:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  word.scan(/^[aeiouAEIOU]/)
+ if word.match(/^[aeiouAEIOU]/)
+   true 
+ else 
+   false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
