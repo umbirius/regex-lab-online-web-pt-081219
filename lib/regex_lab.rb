197e002580d3c3d324ba3dbcf27949ad4781de88
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-19 23:33:44 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+ if word.match(/^[aeiouAEIOU]/)
+   true 
+ else 
+   false
+ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+ if text.match(/^[A-Z].+\W$/)
+  true
+ else
+  false 
+ end 
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/.?\d{3}.?\d{3}.?\d{4}/)
+    true
+  else
+    false
+  end 
 end
